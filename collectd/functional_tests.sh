6ea02940beb35dfd54626c529a25259846dff976
Peter Hardon <peter.hardon@hardynet.cz>
2022-08-08 11:31:34 +0200

    changing interval to 900s for functional tests, shellchecker changes, adding more generated metrics send to prometheus

--- collectd/functional_tests.sh
+++ collectd/functional_tests.sh
@@ -19,13 +19,12 @@ source $rcfile
 hostname|grep "$HOSTNAME_TO_RUN" &>/dev/null || exit 1
 
 #define stackname
-stackname="teststack-`hostname`"
+stackname="teststack-$(hostname)"
 
-HEAT_JSON="`echo $HEAT_JSON|sed s/TESTSTACK/$stackname/g`"
-
+HEAT_JSON="$(echo "$HEAT_JSON"|sed s/TESTSTACK/"$stackname"/g)"
 
 #get token
-OS_TOKEN=`curl -s --fail -i -D - --max-time 10 -o /dev/null \
+OS_TOKEN=$(curl -s --fail -i -D - --max-time 10 -o /dev/null \
   -H "Content-Type: application/json" \
   -d "
 {
@@ -41,10 +40,13 @@ OS_TOKEN=`curl -s --fail -i -D - --max-time 10 -o /dev/null \
         }
     }
 }" \
- $KEYSTONE_ENDPOINT/auth/tokens 2>&1|grep X-Subject-Token`
-[ $? -ne 0 ] && echo "Unable to get token from keystone." 1>&2 && echo "PUTVAL `hostname`/heat/commands-stack interval=60 N:$retcode" && exit 1
+ "$KEYSTONE_ENDPOINT/auth/tokens" 2>&1|grep X-Subject-Token)
+retcode=$?
+echo "PUTVAL $(hostname)/heat/commands-stack_get_token interval=900 N:$retcode" 
+[ $retcode -ne 0 ] && echo "Unable to get token from keystone." 1>&2 && echo "PUTVAL $(hostname)/heat/commands-stack interval=900 N:$retcode" && exit 1
 
-export OS_TOKEN="$(echo $OS_TOKEN |awk '{print $2}'|sed 's/\r$//')"
+OS_TOKEN="$(echo "$OS_TOKEN" |awk '{print $2}'|sed 's/\r$//')"
+export OS_TOKEN
 
 #check if stack already exists
 curl -X GET -g -k -q -s -L --fail -o /dev/null \
@@ -53,7 +55,7 @@ curl -X GET -g -k -q -s -L --fail -o /dev/null \
         -H "Accept: application/json" \
         --max-time 10 \
         --user-agent "curl-healthcheck" \
-        $HEAT_ENDPOINT/$OS_PROJECT_ID/stacks/$stackname
+        "$HEAT_ENDPOINT/$OS_PROJECT_ID/stacks/$stackname"
 retcode=$?
 
 #if stack exist delete it
@@ -65,10 +67,11 @@ then
         -H "Accept: application/json" \
         --max-time 10 \
         --user-agent "curl-healthcheck" \
-        $HEAT_ENDPOINT/$OS_PROJECT_ID/stacks/$stackname
+        "$HEAT_ENDPOINT/$OS_PROJECT_ID/stacks/$stackname"
         retcode=$?
-        [ $retcode -ne 0 ] && echo "Unable to delete stack $stackname." 1>&2 && echo "PUTVAL `hostname`/heat/commands-stack interval=60 N:$retcode" && exit 1
-        #SECONDS is bash special variable, it counts number of seconds from start of the sript,
+        echo "PUTVAL $(hostname)/heat/commands-stack_deletion interval=900 N:$retcode" 
+        [ $retcode -ne 0 ] && echo "Unable to delete stack $stackname." 1>&2 && echo "PUTVAL $(hostname)/heat/commands-stack interval=900 N:$retcode" && exit 1
+        #SECONDS is bash special variable, it counts number of seconds from start of the script,
         # setting to nonnumeric value will reset SECONDS to 0
         SECONDS=reset
         while [ "$SECONDS" -lt $deletion_timeout ]; 
@@ -79,7 +82,7 @@ then
                         -H "Accept: application/json" \
                         --max-time 10 \
                         --user-agent "curl-healthcheck" \
-                        $HEAT_ENDPOINT/$OS_PROJECT_ID/stacks/$stackname || break
+                        "$HEAT_ENDPOINT/$OS_PROJECT_ID/stacks/$stackname" || break
                 echo "Deleting stack. Sleeping 5sec."
                 sleep 5 
         done
@@ -92,9 +95,10 @@ curl -X GET -g -k -q -s -L --fail -o /dev/null \
          -H "Accept: application/json" \
          --max-time 10 \
          --user-agent "curl-healthcheck" \
-         $HEAT_ENDPOINT/$OS_PROJECT_ID/stacks/$stackname
+         "$HEAT_ENDPOINT/$OS_PROJECT_ID/stacks/$stackname"
 retcode=$?
-[ $retcode -eq 0 ] && echo "Stack $stackname hasn't been deleted, still exists." 1>&2 && echo "PUTVAL `hostname`/heat/commands-stack interval=60 N:1" && exit 1
+[ $retcode -ne 0 ] && echo "PUTVAL $(hostname)/heat/commands-stack_deletion interval=900 N:0" || echo "PUTVAL $(hostname)/heat/commands-stack_deletion interval=900 N:1"
+[ $retcode -eq 0 ] && echo "Stack $stackname hasn't been deleted, still exists." 1>&2 && echo "PUTVAL $(hostname)/heat/commands-stack interval=900 N:1" && exit 1
 
 #create stack
 curl -X POST -g -k -q -s --fail -o /dev/null \
@@ -103,31 +107,32 @@ curl -X POST -g -k -q -s --fail -o /dev/null \
       -H "Accept: application/json" \
       --max-time 10 \
       --user-agent "curl-healthcheck" \
-      -d "`echo $HEAT_JSON`" \
-      $HEAT_ENDPOINT/$OS_PROJECT_ID/stacks 
+      -d "$HEAT_JSON" \
+      "$HEAT_ENDPOINT/$OS_PROJECT_ID/stacks"
 retcode=$?
-[ $retcode -ne 0 ] && echo "Unable to create stack. Please check HEAT_JSON variable." 1>&2 && echo "PUTVAL `hostname`/heat/commands-stack interval=60 N:$retcode" && exit 1
+[ $retcode -eq 0 ] && echo "PUTVAL $(hostname)/heat/commands-stack_creation interval=900 N:0" || echo "PUTVAL $(hostname)/heat/commands-stack_creation interval=900 N:1"
+[ $retcode -ne 0 ] && echo "Unable to create stack. Please check HEAT_JSON variable." 1>&2 && echo "PUTVAL $(hostname)/heat/commands-stack interval=900 N:$retcode" && exit 1
 
 #do checks until stack creation
 retcode=1
 SECONDS=reset
 while [ "$SECONDS" -lt $creation_timeout ]; 
 do
-        STACK_STATUS="`curl -X GET -g -k -q -s -L -o - --fail \
-               -H \"X-Auth-Token: $OS_TOKEN\" \
-               -H \"Content-Type: application/json\" \
-               -H \"Accept: application/json\" \
+        STACK_STATUS="$(curl -X GET -g -k -q -s -L -o - --fail \
+               -H "X-Auth-Token: $OS_TOKEN" \
+               -H "Content-Type: application/json" \
+               -H "Accept: application/json" \
                --max-time 10 \
-               --user-agent \"curl-healthcheck\" \
-	       --write-out \"\n\" \
-               $HEAT_ENDPOINT/$OS_PROJECT_ID/stacks/$stackname`" 
-        STACK_STATUS="`echo $STACK_STATUS|python -m json.tool|grep \\"stack_status\\"|awk -F: '{print $2}'|awk -F\\" '{print $2}'`"
-        echo $STACK_STATUS|grep CREATE_COMPLETE &>/dev/null && retcode=0 && break
-        echo $STACK_STATUS|grep CREATE_FAILED &>/dev/null && retcode=1 && break
-        echo $STACK_STATUS|grep ROLLBACK_COMPLETE &>/dev/null && retcode=2 && break
-        echo $STACK_STATUS|grep ROLLBACK_FAILED &>/dev/null && retcode=3 && break
+               --user-agent "curl-healthcheck" \
+               --write-out "\n" \
+               "$HEAT_ENDPOINT/$OS_PROJECT_ID/stacks/$stackname")" 
+        STACK_STATUS="$(echo "$STACK_STATUS"|python3 -m json.tool|grep \"stack_status\"|awk -F: '{print $2}'|awk -F\" '{print $2}')"
+        echo "$STACK_STATUS"|grep CREATE_COMPLETE &>/dev/null && retcode=0 && break
+        echo "$STACK_STATUS"|grep CREATE_FAILED &>/dev/null && retcode=1 && break
+        echo "$STACK_STATUS"|grep ROLLBACK_COMPLETE &>/dev/null && retcode=2 && break
+        echo "$STACK_STATUS"|grep ROLLBACK_FAILED &>/dev/null && retcode=3 && break
         echo "Stack status $STACK_STATUS. Sleeping 10sec."
         sleep 10
 done
-[ $retcode -eq 0 ] && echo "PUTVAL `hostname`/heat/commands-stack interval=60 N:$retcode" && exit 0
-[ $retcode -ne 0 ] && echo "PUTVAL `hostname`/heat/commands-stack interval=60 N:$retcode" && exit 1
+[ $retcode -eq 0 ] && echo "PUTVAL $(hostname)/heat/commands-stack interval=900 N:$retcode" && echo "PUTVAL $(hostname)/heat/commands-stack_creation interval=900 N:$retcode" && exit 0
+[ $retcode -ne 0 ] && echo "PUTVAL $(hostname)/heat/commands-stack interval=900 N:$retcode" && echo "PUTVAL $(hostname)/heat/commands-stack_creation interval=900 N:$retcode" && exit 1
