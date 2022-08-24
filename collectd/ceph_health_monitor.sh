9b19f24c03ac22680530fffb9d21a4c639979c79
Peter Hardon <peter.hardon@hardynet.cz>
2022-08-24 11:07:02 +0200

    fixing line too long in ceph_health_monitor.sh

--- collectd/ceph_health_monitor.sh
+++ collectd/ceph_health_monitor.sh
@@ -12,7 +12,8 @@ source $rcfile
 #if not running from correct host exit the script
 hostname|grep "$HOSTNAME_TO_RUN" &>/dev/null || exit 1
 
-TOKEN="$(curl -s -X POST "http://$CEPH_DASHBOARD_IP_PORT/api/auth" -H  "Accept: application/vnd.ceph.api.v1.0+json" -H  "Content-Type: application/json" -d "{\"username\": \"$CEPH_DASHBOARD_USER\", \"password\": \"$CEPH_DASHBOARD_PASS\"}"|python3 -m json.tool|grep '"token":'|cut -d\" -f4)"
+TOKEN="$(curl -s -X POST "http://$CEPH_DASHBOARD_IP_PORT/api/auth" -H  "Accept: application/vnd.ceph.api.v1.0+json" -H  "Content-Type: application/json" \
+	-d "{\"username\": \"$CEPH_DASHBOARD_USER\", \"password\": \"$CEPH_DASHBOARD_PASS\"}"|python3 -m json.tool|grep '"token":'|cut -d\" -f4)"
 
 curl -s -X GET "http://$CEPH_DASHBOARD_IP_PORT/api/health/minimal" -H  "Content-Type: application/json" -H "Authorization: Bearer $TOKEN"|grep "HEALTH_OK" &>/dev/null
 retcode=$?
