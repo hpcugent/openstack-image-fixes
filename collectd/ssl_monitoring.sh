c21c3c710347e2865a17f4066a83119143e4f217
Peter Hardon <peter.hardon@hardynet.cz>
2023-01-23 11:55:06 +0100

    adding SSL cert check script hostname limitation

--- collectd/ssl_monitoring.sh
+++ collectd/ssl_monitoring.sh
@@ -4,8 +4,12 @@ export rcfile="/opt/configrc"
 
 source $rcfile
 
+[ -z ${HOSTNAME_TO_RUN+x} ] && echo "Variable HOSTNAME_TO_RUN is unset." 1>&2 && exit 1
 [ -z ${SSL_ENDPOINTS_TO_CHECK+x} ] && echo "Variable SSL_ENDPOINTS_TO_CHECK is unset." 1>&2 && exit 1
 [ -z ${SSL_EXPIRATION_IN_NUMBER_OF_SECONDS_CHECK+x} ] && echo "Variable SSL_EXPIRATION_IN_NUMBER_OF_SECONDS_CHECK is unset." 1>&2 && exit 1
+
+#if not running from correct host exit the script
+hostname|grep "$HOSTNAME_TO_RUN" &>/dev/null || exit 1
 
 retcode=0
 for endpoint in $SSL_ENDPOINTS_TO_CHECK
